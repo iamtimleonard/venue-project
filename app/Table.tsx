c2ea9f7def689f580d878c6671584276e6f7a924
iamtimleonard <timleonardcello@gmail.com>
2025-02-26 23:07:58 -0500

    feat: scroll table

--- app/Table.tsx
+++ app/Table.tsx
@@ -1,4 +1,4 @@
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import { Venue } from './api/graphql/types';
 
 type TableProps = {
@@ -7,28 +7,30 @@ type TableProps = {
 
 const TableView: React.FC<TableProps> = ({ venues }) => {
   return (
-    <TableContainer>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell>Open</TableCell>
-            <TableCell>Close</TableCell>
-            <TableCell>Genres</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {venues.map((venue) => (
-            <TableRow key={venue.id}>
-              <TableCell>{venue.name}</TableCell>
-              <TableCell>{venue.dateOpen}</TableCell>
-              <TableCell>{venue.dateClosed || ''}</TableCell>
-              <TableCell>{venue.genres.join(', ')}</TableCell>
+    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Open</TableCell>
+              <TableCell>Close</TableCell>
+              <TableCell>Genres</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {venues.map((venue) => (
+              <TableRow key={venue.id}>
+                <TableCell>{venue.name}</TableCell>
+                <TableCell>{venue.dateOpen}</TableCell>
+                <TableCell>{venue.dateClosed || ''}</TableCell>
+                <TableCell>{venue.genres.join(', ')}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Paper>
   );
 };
 

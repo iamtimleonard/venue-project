40e040d8d7118a8f77862514a727d02e728f831b
iamtimleonard <timleonardcello@gmail.com>
2025-03-09 19:12:53 -0400

    feat: color coordinate by genre

--- app/Points.tsx
+++ app/Points.tsx
@@ -5,20 +5,34 @@ const Points: React.FC<{
   openVenues: number[];
   focusedVenue?: number;
   projection: any;
-}> = ({ points, projection, openVenues, focusedVenue }) =>
-  points.map((venue) => {
-    const fill = openVenues.includes(venue.id) ? 'red' : 'black';
+}> = ({ points, projection, openVenues, focusedVenue }) => {
+  const colors = {
+    jazz: 'blue',
+    rock: 'red',
+    classical: 'blue',
+    EDM: 'purple',
+    house: 'purple',
+    DIY: 'green',
+    rap: 'orange',
+    punk: 'red',
+    folk: 'yellow',
+    'R&B': 'violet',
+    indie: 'red',
+  };
+  return points.map((venue) => {
     return (
       <circle
         cx={projection([venue.longitude, venue.latitude])[0]}
         cy={projection([venue.longitude, venue.latitude])[1]}
         key={venue.id}
         r={venue.id === focusedVenue ? 6 : 3}
-        fill={fill}
+        fill={!openVenues.includes(venue.id) ? 'transparent' : colors[venue.genres[0] || 'black']}
+        stroke={colors[venue.genres[0]] || 'black'}
       >
         <title>{venue.name}</title>
       </circle>
     );
   });
+};
 
 export default Points;
